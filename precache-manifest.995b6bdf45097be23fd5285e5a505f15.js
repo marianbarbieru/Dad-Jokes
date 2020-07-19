85a9076bedcc48910f19a96a3d1ae0715afb4a6e
Marian Barbieru <marianbarbieru@gmail.com>
2020-07-19 20:50:49 +0300

    Updates

---
+++ precache-manifest.995b6bdf45097be23fd5285e5a505f15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dcbaf0113a14b081f53a40548f1dd9f",
+    "url": "/Dad-Jokes/index.html"
+  },
+  {
+    "revision": "35cd6c57c25b292382a6",
+    "url": "/Dad-Jokes/static/css/main.646333cf.chunk.css"
+  },
+  {
+    "revision": "8de430a16a22318e87f3",
+    "url": "/Dad-Jokes/static/js/2.fa116d5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dad-Jokes/static/js/2.fa116d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cd6c57c25b292382a6",
+    "url": "/Dad-Jokes/static/js/main.fe2ef2e6.chunk.js"
+  },
+  {
+    "revision": "7d32b0663e8538b11e8a",
+    "url": "/Dad-Jokes/static/js/runtime-main.add0f485.js"
+  },
+  {
+    "revision": "e64a51de73c984ceb40059b836bfe4d9",
+    "url": "/Dad-Jokes/static/media/smiley.e64a51de.png"
+  }
+]);
